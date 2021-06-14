39bb19c0b56f4b1691bb614f7ccfd139f46712bb
Roman Gnatishen <r.gnatishen@axioma.pl>
2021-06-14 23:33:02 +0200

    Copy rights

--- client_v2/src/containers/TheFooter.vue
+++ client_v2/src/containers/TheFooter.vue
@@ -1,15 +1,15 @@
 <template>
   <CFooter :fixed="false">
     <div>
-      <a href="https://coreui.io" target="_blank">CoreUI</a>
+      <a href="https://axioma.pl" target="_blank">Axioma</a>
       <span class="ml-1"
-        >&copy; {{ new Date().getFullYear() }} creativeLabs.</span
+        >&copy; {{ new Date().getFullYear() }} Synergia.</span
       >
     </div>
-    <div class="mfs-auto">
+    <!-- <div class="mfs-auto">
       <span class="mr-1" target="_blank">Powered by</span>
       <a href="https://coreui.io/vue">CoreUI for Vue</a>
-    </div>
+    </div> -->
   </CFooter>
 </template>
 
