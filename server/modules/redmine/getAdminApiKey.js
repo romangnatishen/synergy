555b1bb85a6e73f13609a09a0630821ed8af5d76
Roman Gnatishen <r.gnatishen@axioma.pl>
2021-06-14 10:38:45 +0200

    filter in users

--- server/modules/redmine/getAdminApiKey.js
+++ server/modules/redmine/getAdminApiKey.js
@@ -1,23 +1,19 @@
 const User = require('../user/model');
 
 module.exports = async () => {
-
-    let admin_api_key = '';
-    const users = await User.findAll(
-        {
+    const filter = {
             where : {
                 email : process.env.REDMINE_ADMIN
             }
-        }
-    );
-    console.log('users', users);
+        };
+
+    let admin_api_key = '';
+    const users = await User.findAll(filter);
     if (users) {
         users.forEach(el => {
             admin_api_key = el.redmine_api_key;            
         });
     }
-    console.log('filter',filter);
-    console.log('admin api key',admin_api_key);
 
     return admin_api_key;
 }
