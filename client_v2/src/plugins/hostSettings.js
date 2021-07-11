bd2be64688182f47fd98758dc2797df7c5b8b237
Roman Gnatishen <r.gnatishen@axioma.pl>
2021-07-12 00:56:55 +0200

    host settings

--- client_v2/src/plugins/hostSettings.js
+++ client_v2/src/plugins/hostSettings.js
@@ -1,6 +1,6 @@
 export default {
-  // DB_HOST: 'https://apisynergy.axioma.pl',
-  DB_HOST: 'http://localhost:3000',
+  DB_HOST: 'https://apisynergy.axioma.pl',
+  //DB_HOST: 'http://localhost:3000',
   HUBSTAFF_HOST: 'http://localhost:3002',
   TASKS_HOST: 'https://tasks.axioma.pl/issues/',
 };
